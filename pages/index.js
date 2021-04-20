8dd0a32ff725fd14c0da61972dfe2a8737c73d13
Ryoo Nishida <cu_0918@yahoo.co.jp>
2021-04-20 10:38:53 +0900

    learn JSX syntax

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,19 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const greeting = "yaa";
+  const dom = <h1> {greeting}</h1>; //h1がトランスパイルされてdomに代入されている
+
   return (
+    //JavaScript XML→JSX　JavaScriptの拡張言語。Reactを用いてHTMLを出力するための言語。
+    //Facebookが開発。XMLやHTMLに見た目は似ている→可読性が高い
+    //JSXは内部でトランスパイルされ、JavaScriptのコードに変換
+    //JSXでもJavaScriptでも挙動は変わらない→JSXの方が容易なため、使われる。
+    //直っ感的にHTMLをかけるという点がJSXの利点。最終的な成果物の形式によりちかい構文で表現できる。
+    //{}で囲うことでJavaScriptを使える
+    //JSX内でのclassの指定通常class JSX上ではclassName(JSの予約語にclassがあるため)
+    //reactは必ず１つのタグしか返せないが、不要なタグを返すことにもなる・・・
+    //それを避けるために、<React.Fragment></React.Fragment>で囲うことで、実質１つのタグにできる。最終的には<React.Fragment>はきえる
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -11,43 +23,12 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello <a href="https://nextjs.org">World!</a>
+          <label>Hello</label>  <label className={styles.text}>World!</label>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+    <input type="text" onClick={() => {console.log("hello")}} />
+    <input type="text" onChange={() => { console.log("hello") }} />
+    <label htmlFor="bar">bar</label>
+      {dom}
       </main>
 
       <footer className={styles.footer}>
